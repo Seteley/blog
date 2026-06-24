c0dbdb604978b9b5ce359e45cd6176c9b8b2ed83
Seteley <tejedasergio2004@gmail.com>
2026-06-24 17:47:30 -0500

    Remove mermaid, disable syntax highlighting, clean code block styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://seteley.github.io',
   base: '/blog',
+  markdown: {
+    syntaxHighlight: false,
+  },
 });
